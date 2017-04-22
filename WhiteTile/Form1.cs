ddf4fe8521b80dc0ff0f0ba19bcbd04ef07d51e1
Алишер Халыкбаев <alisher12398@gmail.com>
2017-04-23 00:08:11 +0600

    3

--- WhiteTile/Form1.cs
+++ WhiteTile/Form1.cs
@@ -7,44 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Drawing.Drawing2D;
 
 namespace WhiteTile
 {
     public partial class Form1 : Form
     {
 
-        Button[] white = new Button[50];
-        Button[] black = new Button[50];
-        Button[] green = new Button[50];
-
+        //Bitmap btm;
+        //public Graphics g;
+        PictureBox[] white = new PictureBox[50];
+        PictureBox[] black = new PictureBox[50];
+        PictureBox[] green = new PictureBox[50];
 
         public Form1()
         {
             InitializeComponent();
-            buttons();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-
-        public void buttons()
-        {
-            for (int i=0; i<50; i++)
+            for (int i = 0; i < 3; i++)
             {
-                white[i] = new Button();
-                white[i].Size = new Size(30, 60);
-                white[i].Location = new Point(100, 100);
+                PictureBox pic2 = new PictureBox();
+                pic2.Size = new Size(100, 200); 
+                pic2.Location = new Point(pic2.Location.X + (i * 100), pic2.Location.Y);
+                //pic2.BackColor = Color.Black;
+                pic2.BackgroundImage = Image.FromFile("black.jpg");
+                black[i] = pic2;
+                Controls.Add(black[i]);
 
-                black[i] = new Button();
-                black[i].Size = new Size(30, 60);
+               /* black[i]  = new PictureBox();
+                black[i].Size = new Size(100, 200);
+                black[i].Image = Image.FromFile("black.jpg");
                 black[i].Location = new Point(200, 200);
-                black[i].BackgroundImage = Image.FromFile("Black.png");
+*/
             }
         }
-        
+
 
 
 
