3780b2caf2e485f2568944ee57d90d36372ce1df
Алишер Халыкбаев <alisher12398@gmail.com>
2017-04-23 19:08:59 +0600

    7

--- WhiteTile/Form1.cs
+++ WhiteTile/Form1.cs
@@ -25,10 +25,12 @@ namespace WhiteTile
         int c;
         int n;
         int x;
+        int i=0;
+        int y1 = 0;
+        int y2 = -100;
         int[] y= new int[12];
        
-        int i;
-
+        
         public Form1()
         {
             InitializeComponent();
@@ -36,37 +38,47 @@ namespace WhiteTile
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+          
          
         }
 
         public void Box2(int c, int i)
         {
-            y[i] = -200;
-            textBox1.Text = n.ToString();
-            textBox2.Text = c.ToString();
+            y[i] = 200;
+            
+            PictureBox pic2 = new PictureBox();
+            PictureBox pic4 = new PictureBox();
+            PictureBox pic1 = new PictureBox();
+            PictureBox pic3 = new PictureBox();
             switch (c)
             {
-
+                                    
                 case 1:
-                    white[i].Size = new Size(100, 200);
-                    white[i].Image = Image.FromFile("white1.jpg");              
-                    white[i].Location = new Point(100, y[i]);
+                    pic1.Size = new Size(100, 200);
+                    pic1.Image = Image.FromFile("green.jpg");
+                    pic1.Location = new Point(100, y[i]);
+                    
+                    white[i] = pic1;
                     Controls.Add(white[i]);
 
-                    white2[i].Size = new Size(100, 200);
-                    white2[i].Image = Image.FromFile("white1.jpg");
-                    white2[i].Location = new Point(200, y[i]);
+                    pic4.Size = new Size(100, 200);
+                    pic4.Image = Image.FromFile("white1.jpg");
+                    pic4.Location = new Point(200, y[i]);
+                    white2[i] = pic4;
                     Controls.Add(white2[i]);
 
-                    white3[i].Size = new Size(100, 200);
-                    white3[i].Image = Image.FromFile("white1.jpg");
-                    white3[i].Location = new Point(300, y[i]);
+                    pic2.Size = new Size(100, 200);
+                    pic2.Image = Image.FromFile("white1.jpg");
+                     pic2.Location = new Point(300, y[i]);
+
+                    white3[i] = pic2;
                     Controls.Add(white3[i]);
 
-                    black[i].Size = new Size(100, 200);
-                    black[i].Image = Image.FromFile("black.jpg");
-                    black[i].Location = new Point(400, y[i]);
+                    pic3.Size = new Size(100, 200);
+                    pic3.Image = Image.FromFile("black.jpg");
+                     pic3.Location = new Point(400, y[i]);
+
+                    black[i] = pic3;
                     Controls.Add(black[i]);
 
                     break;
@@ -259,25 +271,40 @@ namespace WhiteTile
         */
 
 
-        private void Row(int i)
+            private void Row(int i)
         {
             Random c1 = new Random();
             int c2;
             c2 = c1.Next(1, 5);
             int c3 = 1;
-            Box2(c3, i);
+             Box2(c3, i);
+            
         }
 
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            i = 0;
-           
             
-            Row(i);
-            black[i].Location = new Point(400, y[i] + 100);
-            // white[i].Top += 100;
 
+            
+            if (i == 0)
+            {
+                Row(i);
+                textBox1.Text = i.ToString();
+            }
+           
+            i = 2;
+       
+           
+               black[i - 2].Location = new Point(400, y1);
+               white[i - 2].Location = new Point(300, y1);
+               white2[i - 2].Location = new Point(200, y1);
+               white3[i - 2].Location = new Point(100, y1);
+
+               y2 += 100;
+               y1 += 100;
+             
+        
         }
 
 
@@ -293,7 +320,14 @@ namespace WhiteTile
         }
         private void button3_Click(object sender, EventArgs e)
         {
+            PictureBox pic1 = new PictureBox();
+            pic1.Size = new Size(100, 200);
+            pic1.Image = Image.FromFile("white1.jpg");
             
+            pic1.Top = 200;
+            pic1.Left = 100;
+            white[i] = pic1;
+            Controls.Add(white[i]);
         }
         private void button4_Click(object sender, EventArgs e)
         {
